aba5b8920b5f49681efb8af20582e18bf86234cc
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-10-02 12:02:59 +0300

    add app colors

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const browenSugar=Color(0xFFBB7263);
+  static const oldRose=Color(0xFFCE7D6D);
+  static const coralPink=Color(0xFFFA9884);
+  static const melon=Color(0xFFFBA594);
+  static const teaRose=Color(0xFFFDCCC2);
+  static const mistyRose=Color(0xFFFEE6E1);
+  static const white=Color(0xFFFFFFFF);
+
+}
