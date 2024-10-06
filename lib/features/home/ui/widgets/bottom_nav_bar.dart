b1770cc5397e727d9de38dd9b8f603d71e54f1d7
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-10-06 11:58:21 +0300

    add home nav bar

---
+++ lib/features/home/ui/widgets/bottom_nav_bar.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:water_drop_nav_bar/water_drop_nav_bar.dart';
+
+import '../../../../core/theme/colors.dart';
+
+class BottomNavBar extends StatelessWidget {
+  final void Function(int) onItemSelected;
+  final int selectedIndex;
+  const BottomNavBar({super.key, required this.onItemSelected, required this.selectedIndex, });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade200,
+            spreadRadius: 0,
+            blurRadius: 15,
+            offset: const Offset(0, 1), // changes position of shadow
+          ),
+        ],
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(16.r),
+          topRight: Radius.circular(16.r),
+        ),
+      ),
+      child: WaterDropNavBar(
+        bottomPadding: 16.h,
+        inactiveIconColor: AppColors.melon,
+        waterDropColor: AppColors.coralPink,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        onItemSelected:onItemSelected,
+        selectedIndex: selectedIndex,
+        barItems: [
+          BarItem(
+            filledIcon: Icons.bookmark_rounded,
+            outlinedIcon: Icons.bookmark_border_rounded,
+          ),
+          BarItem(
+              filledIcon: Icons.favorite_rounded,
+              outlinedIcon: Icons.favorite_border_rounded),
+          BarItem(
+              filledIcon: Icons.favorite_rounded,
+              outlinedIcon: Icons.favorite_border_rounded),
+          BarItem(
+              filledIcon: Icons.favorite_rounded,
+              outlinedIcon: Icons.favorite_border_rounded),
+        ],
+      ),
+    );
+  }
+}
