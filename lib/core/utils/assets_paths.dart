6112bb741b2f36313d8f29a942d058230f6303d4
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-10-09 07:13:32 +0300

    complete settinngs screen

--- lib/core/utils/assets_paths.dart
+++ lib/core/utils/assets_paths.dart
@@ -1,4 +1,11 @@
 class Assets{
 
   static String onboardingLogo = 'assets/images/onboarding screen logo.png';
+
+  static String profileIcon='assets/svg/profile.svg';
+  static String moreIcon='assets/svg/more.svg';
+  static String likeIcon='assets/svg/Like.svg';
+  static String commentIcon='assets/svg/Comment.svg';
+  static String shareIcon='assets/svg/Share.svg';
+
 }
