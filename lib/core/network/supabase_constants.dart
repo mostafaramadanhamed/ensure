21eb96e2802b113632f3552c9aa75cd92ad9df6a
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-10-13 13:03:28 +0300

    add supabase constants

---
+++ lib/core/network/supabase_constants.dart
@@ -0,0 +1,8 @@
+class SupabaseConstants {
+  static const String supabaseUrl = 'https://aygcufggydhvidqbakqq.supabase.co';
+  static const String supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5Z2N1ZmdneWRodmlkcWJha3FxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc3Njc3MDEsImV4cCI6MjA0MzM0MzcwMX0.Pfi2BCCJbtFVf05mSqSEAETzUEV0d41yBghUnqToBWk';
+
+// collections 
+  static const String usersCollection = 'users';
+  
+}
