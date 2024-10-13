a90dccbfebbd731dd2b5a5f86574ae62bc6d382e
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-10-13 13:03:57 +0300

    add signup cubit and states

---
+++ lib/features/sign up/domain/cubit/sign_up_cubit.dart
@@ -0,0 +1,41 @@
+import 'package:ensure/core/network/auth_exception_handler.dart';
+import 'package:ensure/features/sign%20up/domain/use%20case/signup_use_case.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../data/models/user_model.dart';
+import 'signup_state.dart';
+
+class SignUpCubit extends Cubit<SignupState> {
+  final SignupUseCase signupUseCase;
+  SignUpCubit(this.signupUseCase) : super(SignupInitial());
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  final String profilePic = "";
+
+  void onSignUp() async {
+    if (formKey.currentState!.validate()) {
+      emit(SignupLoading());
+      try {
+        UserModel user = UserModel(
+          name: nameController.text,
+          email: emailController.text,
+          phone: phoneController.text,
+          password: passwordController.text,
+          profileImage: profilePic,
+        );
+        await signupUseCase.signUp(user);
+        emit(SignupSuccess());
+      } catch (e) {
+        
+        emit(SignupFailure(AuthExceptionHandler.parseAuthException(e.toString()).message));
+      }
+    }
+  }
+
+
+}
