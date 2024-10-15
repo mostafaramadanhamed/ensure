9039d6e5d4b72959ec8cd848123c12ee168db899
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-10-15 10:15:22 +0300

    add login states

---
+++ lib/features/login/domain/cubit/cubit/login_state.dart
@@ -0,0 +1,14 @@
+
+
+abstract class LoginState {}
+
+final class LoginInitial extends LoginState {}
+
+final class LoginLoading extends LoginState {}  
+
+final class LoginSuccess extends LoginState {}
+
+final class LoginError extends LoginState {
+  final String message;
+  LoginError({required this.message});
+}
