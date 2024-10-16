dd1665d623d0469f8ffe15b88ff1beeee5b298ab
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-10-16 12:28:02 +0300

    edit paths

---
+++ lib/features/posts/domain/cubit/posts_cubit.dart
@@ -0,0 +1,84 @@
+import 'package:ensure/core/network/auth_exception_handler.dart';
+import 'package:ensure/features/posts/data/models/post_model.dart';
+import 'package:ensure/features/posts/domain/cubit/posts_state.dart';
+import 'package:ensure/features/posts/domain/use%20case/posts_use_case.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class PostsCubit extends Cubit<PostsState> {
+  final PostsUseCase postsUseCase;
+  PostsCubit(this.postsUseCase) : super(PostsInitial());
+
+  final TextEditingController textController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final SupabaseClient supabaseClient = Supabase.instance.client;
+  // add post
+  Future<void> addPost() async {
+    emit(AddPostLoading());
+    try {
+      await postsUseCase.addPost(PostModel(
+        text: textController.text,
+        uId: hashCode,
+        content: '',
+        likes: 0,
+        comments: 0,
+        authorName:
+            supabaseClient.auth.currentUser!.userMetadata!['Display name'],
+        creatdAt: DateTime.now(),
+      ));
+      emit(AddPostSuccess());
+    } catch (e) {
+      emit(AddPostError(e.toString()));
+    }
+  }
+
+  // get posts
+  Future<void> getPosts() async {
+    emit(GetPostsLoading());
+    try {
+      final posts = await postsUseCase.getPosts();
+      debugPrint('data in cubit : $posts');
+      emit(GetPostsSuccess(posts: posts));
+    } catch (e) {
+      emit(GetPostsError(
+          SupanbaseExceptionHandler.parseException(e.toString()).message));
+    }
+  }
+
+  // get posts by user id
+  Future<void> getPostsByUserId(int userId) async {
+    emit(GetPostsLoading());
+    try {
+      final posts = await postsUseCase.getPostsByUserId(userId);
+      emit(GetPostsSuccess(posts: posts));
+    } catch (e) {
+      emit(GetPostsError(
+          SupanbaseExceptionHandler.parseException(e.toString()).message));
+    }
+  }
+
+  // update post
+  Future<void> updatePost(PostModel post) async {
+    emit(UpdatePostLoading());
+    try {
+      await postsUseCase.updatePost(post);
+      emit(UpdatePostSuccess());
+    } catch (e) {
+      emit(UpdatePostError(
+          SupanbaseExceptionHandler.parseException(e.toString()).message));
+    }
+  }
+
+  // delete post
+  Future<void> deletePost(PostModel post) async {
+    emit(DeletePostLoading());
+    try {
+      await postsUseCase.deletePost(post);
+      emit(DeletePostSuccess());
+    } catch (e) {
+      emit(DeletePostError(
+          SupanbaseExceptionHandler.parseException(e.toString()).message));
+    }
+  }
+}
---
+++ lib/features/posts/domain/cubit/posts_state.dart
@@ -0,0 +1,61 @@
+ import '../../data/models/post_model.dart';
+
+abstract class PostsState {}
+
+final class PostsInitial extends PostsState {}
+
+// add posts states
+final class AddPostLoading extends PostsState {}
+
+final class AddPostSuccess extends PostsState {}
+
+final class AddPostError extends PostsState {
+
+  final String message;
+
+  AddPostError(this.message);
+}
+
+// get posts states
+
+final class GetPostsLoading extends PostsState {}
+
+final class GetPostsSuccess extends PostsState {
+
+  final List<PostModel> posts;
+
+  GetPostsSuccess({required this.posts});
+}
+
+final class GetPostsError extends PostsState {
+
+  final String message;
+
+  GetPostsError(this.message);
+}
+
+// update posts states
+
+final class UpdatePostLoading extends PostsState {}
+
+final class UpdatePostSuccess extends PostsState {}
+
+final class UpdatePostError extends PostsState {
+
+  final String message;
+
+  UpdatePostError(this.message);
+}
+
+// delete posts states
+
+final class DeletePostLoading extends PostsState {}
+
+final class DeletePostSuccess extends PostsState {}
+
+final class DeletePostError extends PostsState {
+
+  final String message;
+
+  DeletePostError(this.message);
+}
