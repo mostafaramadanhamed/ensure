ec973fc74d36be7a23ae05b06684870e1c5d26e3
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-10-20 13:47:34 +0300

    edit likes counter

--- lib/features/posts/ui/widgets/post_item.dart
+++ lib/features/posts/ui/widgets/post_item.dart
@@ -23,99 +23,123 @@ class PostItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<PostsCubit, PostsState>(
-      builder: (context, state) {
-        return Card(
-          surfaceTintColor: Colors.grey.shade300,
-          color: Theme.of(context).cardColor,
-          margin: EdgeInsets.symmetric(horizontal: 24.w),
-          elevation: 8,
-          shadowColor: AppColors.melon,
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20.0.r)),
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0.w, vertical: 12.h),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                12.ph,
-                ListTile(
-                  contentPadding: EdgeInsets.zero,
-                  title:
-                      Text(post.authorName, style: TextStyles.font15SemiBold),
-                  leading: Container(
-                    height: 40.h,
-                    width: 40.w,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                            image: NetworkImage(
-                                post.profilePic,),
-                            fit: BoxFit.cover)),
+    return Card(
+      surfaceTintColor: Colors.grey.shade300,
+      color: Theme.of(context).cardColor,
+      margin: EdgeInsets.symmetric(horizontal: 24.w),
+      elevation: 8,
+      shadowColor: AppColors.melon,
+      shape:
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0.r)),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0.w, vertical: 12.h),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            12.ph,
+            ListTile(
+              contentPadding: EdgeInsets.zero,
+              title: Text(post.authorName, style: TextStyles.font15SemiBold),
+              leading: Container(
+                height: 40.h,
+                width: 40.w,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: NetworkImage(
+                          post.profilePic,
+                        ),
+                        fit: BoxFit.cover)),
+              ),
+              trailing: GestureDetector(
+                  onTap: () {}, child: const Icon(Icons.more_horiz_rounded)),
+            ),
+            20.ph,
+            Text(
+              post.text,
+              style: TextStyles.font15SemiBold,
+            ),
+            20.ph,
+            post.content == ''
+                ? Container()
+                : ClipRRect(
+                    borderRadius: BorderRadius.circular(16.0.r),
+                    child: Image.network('https://picsum.photos/400/300',
+                        fit: BoxFit.cover),
                   ),
-                  trailing: GestureDetector(
-                    onTap: () {
-                      
-                      
-                    },
-                    child: const Icon(Icons.more_horiz_rounded)),
-                ),
-                20.ph,
-                Text(
-                  post.text,
-                  style: TextStyles.font15SemiBold,
-                ),
-                20.ph,
-                post.content == ''
-                    ? Container()
-                    : ClipRRect(
-                        borderRadius: BorderRadius.circular(16.0.r),
-                        child: Image.network('https://picsum.photos/400/300',
-                            fit: BoxFit.cover),
-                      ),
-                14.ph,
-                Row(
+            14.ph,
+            BlocBuilder<PostsCubit, PostsState>(
+              builder: (context, state) {
+                // Check if the state contains the likes data
+                bool isLiked = false; // Default to false
+                int likes =
+                    post.likes; // Initialize with the post's current likes
+
+                if (state is LikePostSuccess && state.postId == post.uId) {
+                  // Update likes count if the likes have been updated
+                  likes =
+                      state.likes;
+                                        isLiked = true;
+ // Use the updated likes count from the state
+                } else if (state is IsPostLikedSuccess) {
+                  // Check if the post is liked in the initial state
+                  debugPrint(state.isLiked.toString());
+                  isLiked = state.isLiked;
+                }
+
+                return Row(
                   children: [
                     IconButton(
-                        onPressed: () {
-                          if (context.read<PostsCubit>().isLiked) {
-                            context.read<PostsCubit>().unlikePost(post.uId);
-                          } else {
-                            context.read<PostsCubit>().likePost(post.uId);
-                          }
-                        },
-                        icon: Icon(
-                          Icons.favorite,
-                          color: context.read<PostsCubit>().isLiked
-                              ? Colors.red
-                              : Colors.grey,
-                        )),
-                    Text(
-                      post.likes.toString(),
+                      onPressed: () async {
+                        if (isLiked) {
+                          debugPrint(isLiked.toString());
+                          await context.read<PostsCubit>().unlikePost(post.uId);
+                        } else {
+                          debugPrint(isLiked.toString());
+                          await context
+                              .read<PostsCubit>()
+                              .likePostAndUpdateState(post.uId);
+                        }
+                        // The UI will automatically update due to BlocBuilder
+                      },
+                      icon: Icon(
+                        Icons.favorite,
+                        color: isLiked ? Colors.red : Colors.grey,
+                      ),
+                    ),
+                   likes == 0 ? Container() : Text(
+                      likes.toString(),
                       style: TextStyles.font12LighterBrownBold,
                     ),
-                    12.pw,
+                    8.ph,
                     IconButton(
-                        onPressed: () {
-                          context.pushNamed(Routes.comments,
-                              arguments: post.uId);
-
-                        }, icon: const Icon(Icons.comment)),
-                    Text(post.comments.toString(),
-                        style: TextStyles.font12LighterBrownBold),
-                    12.pw,
-                    IconButton(onPressed: () {}, icon: const Icon(Icons.share)),
-                    const Spacer(),
-                    Text(formatPostTime(post.creatdAt),
-                        style: TextStyles.font12LighterBrownBold),
+                      onPressed: () {
+                        context.pushNamed(Routes.comments, arguments: post.uId);
+                      },
+                      icon: const Icon(Icons.comment),
+                    ),
+                post.comments == 0 ? Container() :    Text(
+                      post.comments.toString(),
+                      style: TextStyles.font12LighterBrownBold,
+                    ),
+                    8.ph,
+                  
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.share),
+                    ),  
+                       const Spacer(),  Text(
+                      formatPostTime(post.creatdAt),
+                      style: TextStyles.font12LighterBrownBold,
+                    ),
+               
                   ],
-                ),
-                14.ph,
-              ],
-            ),
-          ),
-        );
-      },
+                );
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
