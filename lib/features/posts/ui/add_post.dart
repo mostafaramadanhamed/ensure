a4b3e397f762491e987611c9865939fbaeb8fd88
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-10-22 11:29:53 +0300

    remove warning

--- lib/features/posts/ui/add_post.dart
+++ lib/features/posts/ui/add_post.dart
@@ -1,11 +1,8 @@
-import 'package:ensure/core/helpers/navigation_extension.dart';
 import 'package:ensure/core/helpers/spacing_extension.dart';
-import 'package:ensure/core/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../../../core/routing/routes.dart';
 import '../../../core/theme/text_styles.dart';
 import '../../../core/widgets/app_text_button.dart';
 import '../../../core/widgets/app_text_filed.dart';
