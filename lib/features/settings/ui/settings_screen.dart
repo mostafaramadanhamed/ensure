2604fea0f022351ca119a391c3f9b5d53a163f81
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-10-23 11:23:05 +0300

    - logout successfully

--- lib/features/settings/ui/settings_screen.dart
+++ lib/features/settings/ui/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:ensure/core/helpers/navigation_extension.dart';
+import 'package:ensure/core/helpers/shared_pref_helper.dart';
 import 'package:ensure/core/helpers/spacing_extension.dart';
 import 'package:ensure/core/routing/routes.dart';
 import 'package:ensure/core/utils/assets_paths.dart';
@@ -26,6 +27,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
 
     context.read<PostsCubit>().getUserDetails();
   }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -36,85 +38,112 @@ class _SettingsScreenState extends State<SettingsScreen> {
       ),
       child: BlocBuilder<PostsCubit, PostsState>(
         builder: (context, state) {
-
-        if(state is GetUserDetailsSuccess)  {return ListView(
-            padding: EdgeInsets.zero,
-            physics: const BouncingScrollPhysics(),
-            children: [
-              DisplayUserName(user: state.user,),
-              24.ph,
-              SettingItem(
-                title: 'Profile',
-                onTap: () {
-                  context.pushNamed(Routes.profile);
-                },
-                widgetLeading: SvgPicture.asset(
-                  Assets.profileIcon,
-                  colorFilter: ColorFilter.mode(
-                      IconTheme.of(context).color!, BlendMode.srcIn),
+          if (state is GetUserDetailsSuccess) {
+            return ListView(
+              padding: EdgeInsets.zero,
+              physics: const BouncingScrollPhysics(),
+              children: [
+                DisplayUserName(
+                  user: state.user,
                 ),
-              ),
-              16.ph,
-              SettingItem(
-                title: 'Dark Mode',
-                onTap: () {},
-                widgetLeading: const Icon(Icons.dark_mode_rounded),
-              ),
-              16.ph,
-              SettingItem(
-                title: 'Language',
-                onTap: () {},
-                widgetLeading: const Icon(Icons.language_rounded),
-              ),
-              16.ph,
-              SettingItem(
-                title: 'Notifications',
-                onTap: () {},
-                widgetLeading: const Icon(Icons.notifications_outlined),
-              ),
-              16.ph,
-              SettingItem(
-                title: 'About',
-                onTap: () {},
-                widgetLeading: const Icon(Icons.info_outline_rounded),
-              ),
-              16.ph,
-              SettingItem(
-                title: 'Help & Support',
-                onTap: () {},
-                widgetLeading: const Icon(Icons.help_outline_rounded),
-              ),
-              16.ph,
-              SettingItem(
-                title: 'Rate the app',
-                onTap: () {},
-                widgetLeading: const Icon(Icons.star_outline_rounded),
-              ),
-              16.ph,
-              SettingItem(
-                title: 'Privacy policy',
-                onTap: () {},
-                widgetLeading: const Icon(Icons.privacy_tip_rounded),
-              ),
-              16.ph,
-              SettingItem(
-                title: 'Logout',
-                onTap: () {},
-                widgetLeading: const Icon(Icons.logout_rounded),
-              ),
-            ],
+                24.ph,
+                SettingItem(
+                  title: 'Profile',
+                  onTap: () {
+                    context.pushNamed(Routes.profile);
+                  },
+                  widgetLeading: SvgPicture.asset(
+                    Assets.profileIcon,
+                    colorFilter: ColorFilter.mode(
+                        IconTheme.of(context).color!, BlendMode.srcIn),
+                  ),
+                ),
+                16.ph,
+                SettingItem(
+                  title: 'Dark Mode',
+                  onTap: () {},
+                  widgetLeading: const Icon(Icons.dark_mode_rounded),
+                ),
+                16.ph,
+                SettingItem(
+                  title: 'Language',
+                  onTap: () {},
+                  widgetLeading: const Icon(Icons.language_rounded),
+                ),
+                16.ph,
+                SettingItem(
+                  title: 'Notifications',
+                  onTap: () {},
+                  widgetLeading: const Icon(Icons.notifications_outlined),
+                ),
+                16.ph,
+                SettingItem(
+                  title: 'About',
+                  onTap: () {},
+                  widgetLeading: const Icon(Icons.info_outline_rounded),
+                ),
+                16.ph,
+                SettingItem(
+                  title: 'Help & Support',
+                  onTap: () {},
+                  widgetLeading: const Icon(Icons.help_outline_rounded),
+                ),
+                16.ph,
+                SettingItem(
+                  title: 'Rate the app',
+                  onTap: () {},
+                  widgetLeading: const Icon(Icons.star_outline_rounded),
+                ),
+                16.ph,
+                SettingItem(
+                  title: 'Privacy policy',
+                  onTap: () {},
+                  widgetLeading: const Icon(Icons.privacy_tip_rounded),
+                ),
+                16.ph,
+                SettingItem(
+                  title: 'Logout',
+                  onTap: () async {
+                    showDialog(
+                        context: context,
+                        builder: (_) {
+                          return AlertDialog(
+                            title: const Text('Logout'),
+                            content:
+                                const Text('Are you sure you want to logout?'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('No'),
+                              ),
+                              TextButton(
+                                onPressed: () async {
+                                  context.read<PostsCubit>().logout();
+                                  SharedPrefHelper.clearAllSecuredData();
+                                  context.pushReplacementNamed(Routes.login);
+                                },
+                                child: const Text('Yes'),
+                              ),
+                            ],
+                          );
+                        });
+                  },
+                  widgetLeading: const Icon(Icons.logout_rounded),
+                ),
+              ],
+            );
+          }
+          if (state is GetUserDetailsError) {
+            return Center(
+              child: Text(state.message),
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
           );
-      } 
-      if(state is GetUserDetailsError) {
-        return Center(
-          child: Text(state.message),
-        );
-      }
-      return const Center(
-        child: CircularProgressIndicator(),
-      );
-       },
-       
+        },
       ),
     );
   }
