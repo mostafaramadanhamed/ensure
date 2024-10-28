2cb168b30bce52ba1e9e02ec2eddd9d4a401bca3
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-10-28 11:32:52 +0300

    create search model

---
+++ lib/features/search/data/models/search_model.dart
@@ -0,0 +1,11 @@
+class SearchModel {
+  final int userId;
+  final String userName;
+  final String userImage;
+
+  SearchModel({
+    required this.userId,
+    required this.userName,
+    required this.userImage,
+  });
+}
