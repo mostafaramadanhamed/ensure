6f9693527172b6d01dd042edc4fd23fc8aa9dbfa
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-11-06 10:48:08 +0200

    - added conversation id

--- lib/features/chat/data/models/conversation_model.dart
+++ lib/features/chat/data/models/conversation_model.dart
@@ -1,14 +1,14 @@
 class ConversationModel {
-  final int id;
+  final int conversationId;
   final String user1Id;
   final String user2Id;
   final DateTime createdAt;
 
-  ConversationModel({ required this.id, required this.user1Id, required this.user2Id, required this.createdAt});
+  ConversationModel({ required this.conversationId, required this.user1Id, required this.user2Id, required this.createdAt});
 
   factory ConversationModel.fromJson(Map<String, dynamic> json) {
     return ConversationModel(
-      id: json['id'],
+      conversationId: json['conversation_id'],
       user1Id: json['user1_id'],
       user2Id: json['user2_id'],
       createdAt: DateTime.parse(json['created_at']),
