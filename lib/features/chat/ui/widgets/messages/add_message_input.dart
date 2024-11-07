c0d2d06e01439e31c214f021566737d9586da94e
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-11-07 10:53:38 +0200

    - message controller and send message in add message input

--- lib/features/chat/ui/widgets/messages/add_message_input.dart
+++ lib/features/chat/ui/widgets/messages/add_message_input.dart
@@ -1,12 +1,16 @@
 import 'package:ensure/core/helpers/spacing_extension.dart';
 import 'package:ensure/core/widgets/app_text_filed.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../../core/theme/colors.dart';
+import '../../../domain/cubit/chat_cubit.dart';
 
 class AddMessageInput extends StatelessWidget {
-  const AddMessageInput({super.key});
+  final int conversationId;
+  final String receiverId;
+  const AddMessageInput({super.key, required this.conversationId, required this.receiverId});
 
   @override
   Widget build(BuildContext context) {
@@ -16,18 +20,27 @@ class AddMessageInput extends StatelessWidget {
         padding:  EdgeInsets.symmetric(horizontal: 8.w,vertical: 4.h),
         child: Row(
           children: [
-            const Expanded(
+             Expanded(
               child: 
                    AppTextFormField(
+                    controller: context.read<ChatCubit>().messageController,
                     hintText: "Enter your message",
                                    ),
                 
             ),
             
             16.pw,
-            const Icon(
-              Icons.send_outlined,
-              color: AppColors.coralPink,
+            GestureDetector(
+              onTap: (){
+                context.read<ChatCubit>().sendMessage(
+                  receiverId: receiverId,
+                  conversationId: conversationId,
+                );
+              },
+              child: const Icon(
+                Icons.send_outlined,
+                color: AppColors.coralPink,
+              ),
             ),
         
         ]
