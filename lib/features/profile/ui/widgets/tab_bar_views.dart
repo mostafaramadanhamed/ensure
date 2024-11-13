a2e35fa3aa78ada852e8e8dbf04077bcf8bba1f4
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-11-13 11:48:06 +0200

    added shimmer loading for profile posts

--- lib/features/profile/ui/widgets/tab_bar_views.dart
+++ lib/features/profile/ui/widgets/tab_bar_views.dart
@@ -1,4 +1,5 @@
 import 'package:ensure/core/helpers/spacing_extension.dart';
+import 'package:ensure/features/posts/ui/widgets/shimmer_post_loading.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -7,40 +8,45 @@ import '../../domain/cubit/profile_cubit.dart';
 import 'profile_post_item.dart';
 
 class PostsPorfile extends StatelessWidget {
-    final String userId; // Pass the user ID to fetch data for specific user
-
+  final String userId; // Pass the user ID to fetch data for specific user
 
   const PostsPorfile({
-    super.key, required this.userId,
+    super.key,
+    required this.userId,
   });
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<PostModel>>(
-        future: context.read<ProfileCubit>().getPostsByUserId(userId), // Fetch posts based on user ID
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          }
-          if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('No Posts'));
-          }
-    
-          final posts = snapshot.data!;
-          return ListView.separated(
-            separatorBuilder: (context, index) => 16.ph,
-            padding: EdgeInsets.zero,
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: posts.length,
-            itemBuilder: (context, index) {
-              return ProfilePostItem(post: posts[index],userId: userId,);
-            },
-          );
-        },
-      );
+      future: context
+          .read<ProfileCubit>()
+          .getPostsByUserId(userId), // Fetch posts based on user ID
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const ShimmerPostLoading();
+        }
+        if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        }
+        if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(child: Text('No Posts'));
+        }
+
+        final posts = snapshot.data!;
+        return ListView.separated(
+          separatorBuilder: (context, index) => 16.ph,
+          padding: EdgeInsets.zero,
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: posts.length,
+          itemBuilder: (context, index) {
+            return ProfilePostItem(
+              post: posts[index],
+              userId: userId,
+            );
+          },
+        );
+      },
+    );
   }
 }
