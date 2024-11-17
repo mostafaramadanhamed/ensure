6c7d2ab7f54dba169c1b37d5676da0cc1bc4a52a
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-11-17 09:00:26 +0200

    - added main function comment

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,21 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'core/di/dependency_injection.dart';
 import 'core/routing/app_router.dart';
 
+/// The main function of the application.
+///
+/// This function does the following:
+///
+/// 1. Ensures the widgets binding is initialized.
+/// 2. Ensures the screen size is initialized.
+/// 3. Sets up the dependency injection.
+/// 4. Initializes the easy localization.
+/// 5. Sets the bloc observer.
+/// 6. Checks if the user is logged in.
+/// 7. Sets the default locale to 'en_US'.
+/// 8. Runs the application with the easy localization wrapper.
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
   await ScreenUtil.ensureScreenSize();
     await setupGetIt();
   await EasyLocalization.ensureInitialized();
@@ -17,6 +30,8 @@ void main() async {
     Bloc.observer = AppBlocObserver();
 
     await checkIfLoggedInUser();
+    // for disabling date time localization
+    // display date and time in english
     Intl.defaultLocale = 'en_US';
 
   
